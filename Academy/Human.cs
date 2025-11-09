d08103fbc49bffcca583222ea79361963b9a5f96
Admin <Admin@DESKTOP-8URQBFL>
2025-11-09 18:20:36 +0500

    Add project files.

---
+++ Academy/Human.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Human
+	{
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public int Age { get; set; }
+
+		public Human(string lastName,  string firstName, int age)
+		{
+			LastName = lastName;
+			FirstName = firstName;
+			Age = age;
+
+			Console.WriteLine($"HConstructor:\t{GetHashCode()}");
+		}
+
+		public Human(Human other)
+		{
+			this.LastName = other.LastName;	
+			this.FirstName = other.FirstName;
+			this.Age = other.Age;
+			Console.WriteLine($"CopyConstructor:\t{GetHashCode()}");
+		}
+		~Human()
+		{
+			Console.WriteLine($"HDestructor:\t{GetHashCode()}");
+		}
+
+		public virtual void Info()
+		{
+			Console.WriteLine($"{LastName} {FirstName} {Age}");
+		}
+		public override string ToString()
+		{
+			return $"{base.ToString().Split('.').Last()}:".PadRight(12) +
+				$" {LastName.PadRight(16)} {FirstName.PadRight(16)} {Age.ToString().PadRight(5)}";
+		}
+		public virtual string ToStringCSV()
+		{
+			return 
+				this.GetType().ToString().Split('.').Last() + ","
+				+ $"{LastName},{FirstName},{Age}";
+		}
+		public virtual Human Init(string[] values)
+		{
+			LastName = values[1];
+			FirstName = values[2];
+			Age = Convert.ToInt32(values[3]);
+			return this;
+		}
+	}
+}
---
+++ Academy/Program.cs
@@ -0,0 +1,134 @@
+﻿//#define INHERITANCE_1
+//#define INHERITANCE_2
+//#define WRITE_TO_FILE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Academy
+{
+	internal class Program
+	{
+		static readonly string delimiter = "\n---------------------------------------\n";
+		static void Main(string[] args)
+		{
+#if INHERITANCE_1
+			Console.WriteLine("Academy");
+			Human human = new Human("Montana", "Antonio", 25);
+			human.Info();
+			Console.WriteLine(delimiter);
+
+			Student student = new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 90, 95);
+			student.Info();
+			Console.WriteLine(delimiter);
+
+			Teacher teacher = new Teacher("White", "Walter", 50, "Chemistry", 25);
+			teacher.Info();
+			Console.WriteLine(delimiter); 
+#endif
+
+#if INHERITANCE_2
+			Human human = new Human("Pinkman", "Jessie", 22);
+			human.Info();
+			Console.WriteLine(delimiter);
+
+			Student student = new Student(human, "Chemistry", "WW_220", 90, 95);
+			student.Info();
+			Console.WriteLine(delimiter);
+
+			Teacher teacher = new Teacher(new Human("White", "Walter", 50), "Chemistry", 25);
+			teacher.Info();
+			Console.WriteLine(delimiter);
+
+			Human h_hank = new Human("Schreder", "Hank", 40);
+			Student s_hank = new Student(h_hank, "Criminalistic", "OBN", 50, 60);
+			Graduate graduate = new Graduate(s_hank, "How to catch Heisenberg");
+			graduate.Info();
+#endif
+
+#if WRITE_TO_FILE
+			Human[] group =
+				{
+					new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 90, 95),
+					new Teacher("White", "Walter", 50, "Chemistry", 25),
+					new Graduate("Schreder", "Hank", 40, "Criminalistic", "OBN", 50, 60, "How to catch Heisenberg"),
+					new Student("Vercetty", "Tommy", 30, "Theft", "Vice", 98, 99),
+					new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20),
+					new Teacher("Schwazeneger", "Arnold", 78, "Heavy Metal", 65)
+			};
+			Console.WriteLine(delimiter);
+
+			for (int i = 0; i < group.Length; i++)
+			{
+				//group[i].Info();
+				Console.WriteLine(group[i]);
+				Console.WriteLine(delimiter);
+			}
+			Save(group, "group.txt"); 
+#endif
+
+			Human[] group = Load("group.txt");
+			Print(group);
+
+		}
+		static void Save(Human[] group, string filename)
+		{
+			StreamWriter writer = new StreamWriter(filename);
+			for (int i = 0; i < group.Length; i++)
+			{
+				writer.WriteLine(group[i].ToStringCSV());
+			}
+			writer.Close();
+			System.Diagnostics.Process.Start("notepad", filename);
+		}
+		static void Print(Human[] group)
+		{
+			for (int i = 0; i < group.Length; i++)
+			{
+				Console.WriteLine(group[i]);
+				Console.WriteLine(delimiter);
+			}
+			Console.WriteLine();
+		}
+
+		static Human[] Load(string filename)
+		{
+			List<Human> group = new List<Human>();
+			StreamReader reader = new StreamReader(filename);
+			try
+			{
+				while (!reader.EndOfStream)
+				{
+					string buffer = reader.ReadLine();
+					string[] values = buffer.Split(',');
+					//Human human = HumanFactory(values.First());
+					//human.Init(values);
+					//group.Add(human);
+					group.Add(HumanFactory(values[0]).Init(values));
+				}
+			}
+			catch (Exception ex) 
+			{ 
+				Console.WriteLine(ex.Message);
+			}
+
+			reader.Close();
+			return group.ToArray();
+		}
+		static Human HumanFactory(string type)
+		{
+			Human human = null;
+			switch (type)
+			{
+				case "Human": human = new Human("", "", 0); break;
+				case "Student": human = new Student("", "", 0, "", "", 0, 0); break;
+				case "Graduate": human = new Graduate("", "", 0, "", "", 0, 0, ""); break;
+				case "Teacher": human = new Teacher("", "", 0, "", 0); break;
+			}
+			return human;
+		}
+	}
+}
